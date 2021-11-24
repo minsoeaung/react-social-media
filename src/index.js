36e92296ac04cd27b94706ab69ee9822e41d1dfa
minsoeaung <minsoeaung2001@gmail.com>
2021-11-24 19:46:33 +0630

    Login and Register view

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Profile from "./views/Profile/Profile";
 import Home from "./views/Home/Home";
+import Login from "./views/Login/Login";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -17,6 +18,8 @@ ReactDOM.render(
                     <Route index element={<Home/>}/>
                     <Route path="/profile/:username" element={<Profile/>}/>
                 </Route>
+
+                <Route path="/login" element={<Login/>}/>
 
                 {/*  no match route  */}
                 <Route path="*" element={<main><p>There's nothing here.</p></main>}/>
